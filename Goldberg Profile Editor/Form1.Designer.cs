64612b24ee466b6434647b1cf55be40d3876f006
BTFighter <45516471+BTFighter@users.noreply.github.com>
2025-07-14 18:00:15 +0800

    Initial commit

---
+++ Goldberg Profile Editor/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Goldberg_Profile_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.avatarPanel = new System.Windows.Forms.Panel();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.steamIDTextBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.languageLabel = new System.Windows.Forms.Label();
+            this.languageComboBox = new System.Windows.Forms.ComboBox();
+            this.changeSettingsFolderButton = new System.Windows.Forms.Button();
+            this.settingsFolderTextBox = new System.Windows.Forms.TextBox();
+            this.revertSettingsFolderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // avatarPanel
+            // 
+            this.avatarPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.avatarPanel.Location = new System.Drawing.Point(12, 31);
+            this.avatarPanel.Name = "avatarPanel";
+            this.avatarPanel.Size = new System.Drawing.Size(184, 184);
+            this.avatarPanel.TabIndex = 0;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(211, 113);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(195, 20);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // steamIDTextBox
+            // 
+            this.steamIDTextBox.Location = new System.Drawing.Point(433, 113);
+            this.steamIDTextBox.Name = "steamIDTextBox";
+            this.steamIDTextBox.Size = new System.Drawing.Size(195, 20);
+            this.steamIDTextBox.TabIndex = 2;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(208, 97);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(55, 13);
+            this.userLabel.TabIndex = 3;
+            this.userLabel.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Steam ID";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(489, 170);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(59, 25);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Click to Change Avatar";
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.AutoSize = true;
+            this.languageLabel.Location = new System.Drawing.Point(208, 154);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(55, 13);
+            this.languageLabel.TabIndex = 7;
+            this.languageLabel.Text = "Language";
+            // 
+            // languageComboBox
+            // 
+            this.languageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.languageComboBox.FormattingEnabled = true;
+            this.languageComboBox.Location = new System.Drawing.Point(211, 170);
+            this.languageComboBox.Name = "languageComboBox";
+            this.languageComboBox.Size = new System.Drawing.Size(195, 21);
+            this.languageComboBox.TabIndex = 8;
+            // 
+            // changeSettingsFolderButton
+            // 
+            this.changeSettingsFolderButton.Location = new System.Drawing.Point(323, 60);
+            this.changeSettingsFolderButton.Name = "changeSettingsFolderButton";
+            this.changeSettingsFolderButton.Size = new System.Drawing.Size(195, 25);
+            this.changeSettingsFolderButton.TabIndex = 9;
+            this.changeSettingsFolderButton.Text = "Open Settings Folder";
+            this.changeSettingsFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // settingsFolderTextBox
+            // 
+            this.settingsFolderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.settingsFolderTextBox.Location = new System.Drawing.Point(265, 34);
+            this.settingsFolderTextBox.Name = "settingsFolderTextBox";
+            this.settingsFolderTextBox.ReadOnly = false;
+            this.settingsFolderTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.settingsFolderTextBox.Size = new System.Drawing.Size(300, 20);
+            this.settingsFolderTextBox.TabIndex = 10;
+            this.settingsFolderTextBox.TabStop = false;
+            this.settingsFolderTextBox.WordWrap = false;
+            // 
+            // revertSettingsFolderButton
+            // 
+            this.revertSettingsFolderButton.Location = new System.Drawing.Point(570, 33);
+            this.revertSettingsFolderButton.Name = "revertSettingsFolderButton";
+            this.revertSettingsFolderButton.Size = new System.Drawing.Size(60, 22);
+            this.revertSettingsFolderButton.TabIndex = 11;
+            this.revertSettingsFolderButton.Text = "Revert";
+            this.revertSettingsFolderButton.UseVisualStyleBackColor = true;
+            this.revertSettingsFolderButton.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 265);
+            this.Controls.Add(this.revertSettingsFolderButton);
+            this.Controls.Add(this.settingsFolderTextBox);
+            this.Controls.Add(this.changeSettingsFolderButton);
+            this.Controls.Add(this.languageComboBox);
+            this.Controls.Add(this.languageLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.steamIDTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.avatarPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Goldberg Profile Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel avatarPanel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox steamIDTextBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label languageLabel;
+        private System.Windows.Forms.ComboBox languageComboBox;
+        private System.Windows.Forms.Button changeSettingsFolderButton;
+        private System.Windows.Forms.TextBox settingsFolderTextBox;
+        private System.Windows.Forms.Button revertSettingsFolderButton;
+    }
+}
+
---
+++ Goldberg Profile Editor/Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Goldberg_Profile_Editor
+{
+    public partial class Form1 : Form
+    {
+        private string settingsFolderPath;
+        private string configPathFile;
+        private string defaultSettingsFolder;
+        private string exeConfigPathFile;
+        private string originalSettingsFolderPath;
+        private bool isEditingSettingsFolder = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            avatarPanel.Cursor = Cursors.Hand;
+            exeConfigPathFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config_path.cfg");
+            defaultSettingsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "GSE Saves", "settings");
+            settingsFolderPath = GetSettingsFolderPath();
+            originalSettingsFolderPath = settingsFolderPath;
+            changeSettingsFolderButton.Click += ChangeSettingsFolderButton_Click;
+            settingsFolderTextBox.TextChanged += SettingsFolderTextBox_TextChanged;
+            revertSettingsFolderButton.Click += RevertSettingsFolderButton_Click;
+            UpdateSettingsFolderTextBox();
+            LoadUserAvatar();
+            InitializeLanguageComboBox();
+            LoadUserInfo();
+            saveButton.Click += SaveButton_Click;
+            avatarPanel.Click += AvatarPanel_Click;
+        }
+
+        private string GetSettingsFolderPath()
+        {
+            try
+            {
+                if (File.Exists(exeConfigPathFile))
+                {
+                    string customPath = File.ReadAllText(exeConfigPathFile).Trim();
+                    if (!string.IsNullOrEmpty(customPath) && Directory.Exists(customPath))
+                        return customPath;
+                }
+            }
+            catch { }
+            return defaultSettingsFolder;
+        }
+
+        private void SetSettingsFolderPath(string newPath)
+        {
+            if (!string.IsNullOrEmpty(newPath) && newPath != defaultSettingsFolder)
+            {
+                Directory.CreateDirectory(newPath);
+                File.WriteAllText(exeConfigPathFile, newPath);
+                settingsFolderPath = newPath;
+            }
+            else
+            {
+                settingsFolderPath = defaultSettingsFolder;
+            }
+            UpdateSettingsFolderTextBox();
+        }
+
+        private void UpdateSettingsFolderTextBox()
+        {
+            if (File.Exists(exeConfigPathFile))
+                settingsFolderTextBox.Text = settingsFolderPath;
+            else
+                settingsFolderTextBox.Text = defaultSettingsFolder;
+            originalSettingsFolderPath = settingsFolderTextBox.Text;
+            isEditingSettingsFolder = false;
+            changeSettingsFolderButton.Text = "Open Settings Folder";
+            revertSettingsFolderButton.Visible = false;
+        }
+
+        private void SettingsFolderTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (settingsFolderTextBox.Text != originalSettingsFolderPath)
+            {
+                isEditingSettingsFolder = true;
+                changeSettingsFolderButton.Text = "Save";
+                revertSettingsFolderButton.Visible = true;
+            }
+            else
+            {
+                isEditingSettingsFolder = false;
+                changeSettingsFolderButton.Text = "Open Settings Folder";
+                revertSettingsFolderButton.Visible = false;
+            }
+        }
+
+        private void RevertSettingsFolderButton_Click(object sender, EventArgs e)
+        {
+            settingsFolderTextBox.Text = originalSettingsFolderPath;
+            isEditingSettingsFolder = false;
+            changeSettingsFolderButton.Text = "Open Settings Folder";
+            revertSettingsFolderButton.Visible = false;
+        }
+
+        private void ChangeSettingsFolderButton_Click(object sender, EventArgs e)
+        {
+            if (isEditingSettingsFolder)
+            {
+                string newPath = settingsFolderTextBox.Text.Trim();
+                if (!string.IsNullOrEmpty(newPath))
+                {
+                    SetSettingsFolderPath(newPath);
+                    originalSettingsFolderPath = settingsFolderPath;
+                    isEditingSettingsFolder = false;
+                    changeSettingsFolderButton.Text = "Open Settings Folder";
+                    revertSettingsFolderButton.Visible = false;
+                    // Only create config if not default
+                    if (settingsFolderPath != defaultSettingsFolder)
+                    {
+                        string iniPath = Path.Combine(settingsFolderPath, "configs.user.ini");
+                        if (!File.Exists(iniPath))
+                        {
+                            File.WriteAllText(iniPath, "account_name=\naccount_steamid=\nlanguage=english\n");
+                        }
+                    }
+                    MessageBox.Show($"Settings folder changed to:\n{settingsFolderPath}", "Settings Folder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadUserAvatar();
+                    LoadUserInfo();
+                }
+            }
+            else
+            {
+                // Open folder in explorer
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", settingsFolderPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Could not open folder: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void InitializeLanguageComboBox()
+        {
+            var languages = new List<(string Display, string Code)>
+            {
+                ("Arabic", "arabic"),
+                ("Bulgarian", "bulgarian"),
+                ("Chinese (Simplified)", "schinese"),
+                ("Chinese (Traditional)", "tchinese"),
+                ("Czech", "czech"),
+                ("Danish", "danish"),
+                ("Dutch", "dutch"),
+                ("English", "english"),
+                ("Finnish", "finnish"),
+                ("French", "french"),
+                ("German", "german"),
+                ("Greek", "greek"),
+                ("Hungarian", "hungarian"),
+                ("Indonesian", "indonesian"),
+                ("Italian", "italian"),
+                ("Japanese", "japanese"),
+                ("Korean", "koreana"),
+                ("Norwegian", "norwegian"),
+                ("Polish", "polish"),
+                ("Portuguese", "portuguese"),
+                ("Portuguese-Brazil", "brazilian"),
+                ("Romanian", "romanian"),
+                ("Russian", "russian"),
+                ("Spanish-Spain", "spanish"),
+                ("Spanish-Latin America", "latam"),
+                ("Swedish", "swedish"),
+                ("Thai", "thai"),
+                ("Turkish", "turkish"),
+                ("Ukrainian", "ukrainian"),
+                ("Vietnamese", "vietnamese")
+            };
+            languageComboBox.Items.Clear();
+            foreach (var lang in languages)
+            {
+                languageComboBox.Items.Add(new ComboBoxItem(lang.Display, lang.Code));
+            }
+        }
+
+        private class ComboBoxItem
+        {
+            public string Display { get; }
+            public string Code { get; }
+            public ComboBoxItem(string display, string code)
+            {
+                Display = display;
+                Code = code;
+            }
+            public override string ToString() => Display;
+        }
+
+        private void LoadUserAvatar()
+        {
+            try
+            {
+                // Use settingsFolderPath
+                if (!Directory.Exists(settingsFolderPath))
+                {
+                    SetDefaultAvatar();
+                    return;
+                }
+
+                // Look for avatar files with different extensions
+                string[] possibleExtensions = { ".png", ".jpg", ".jpeg" };
+                string avatarPath = null;
+
+                foreach (string extension in possibleExtensions)
+                {
+                    string testPath = Path.Combine(settingsFolderPath, "account_avatar" + extension);
+                    if (File.Exists(testPath))
+                    {
+                        avatarPath = testPath;
+                        break;
+                    }
+                }
+
+                if (avatarPath != null)
+                {
+                    using (Image originalImage = Image.FromFile(avatarPath))
+                    {
+                        Bitmap resizedAvatar = new Bitmap(originalImage, new Size(184, 184));
+                        avatarPanel.BackgroundImage = resizedAvatar;
+                        avatarPanel.BackgroundImageLayout = ImageLayout.Center;
+                        avatarPanel.Size = new Size(184, 184);
+                    }
+                }
+                else
+                {
+                    SetDefaultAvatar();
+                }
+            }
+            catch (Exception ex)
+            {
+                SetDefaultAvatar();
+                MessageBox.Show($"Error loading avatar: {ex.Message}", "Avatar Load Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void SetDefaultAvatar()
+        {
+            // Create a default avatar (gray placeholder)
+            Bitmap defaultAvatar = new Bitmap(184, 184);
+            using (Graphics g = Graphics.FromImage(defaultAvatar))
+            {
+                g.Clear(Color.LightGray);
+                g.DrawString("No Avatar", new Font("Arial", 12), Brushes.DarkGray, 50, 80);
+            }
+            
+            avatarPanel.BackgroundImage = defaultAvatar;
+            avatarPanel.BackgroundImageLayout = ImageLayout.Center;
+            avatarPanel.Size = new Size(184, 184);
+        }
+
+        private void LoadUserInfo()
+        {
+            try
+            {
+                string iniPath = Path.Combine(settingsFolderPath, "configs.user.ini");
+                if (!File.Exists(iniPath))
+                    return;
+                string[] lines = File.ReadAllLines(iniPath);
+                foreach (string line in lines)
+                {
+                    string trimmed = line.Trim();
+                    if (trimmed.StartsWith("account_name="))
+                    {
+                        usernameTextBox.Text = trimmed.Substring("account_name=".Length);
+                    }
+                    else if (trimmed.StartsWith("account_steamid="))
+                    {
+                        steamIDTextBox.Text = trimmed.Substring("account_steamid=".Length);
+                    }
+                    else if (trimmed.StartsWith("language="))
+                    {
+                        string langCode = trimmed.Substring("language=".Length);
+                        foreach (ComboBoxItem item in languageComboBox.Items)
+                        {
+                            if (item.Code == langCode)
+                            {
+                                languageComboBox.SelectedItem = item;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (languageComboBox.SelectedIndex == -1 && languageComboBox.Items.Count > 0)
+                {
+                    foreach (ComboBoxItem item in languageComboBox.Items)
+                    {
+                        if (item.Code == "english")
+                        {
+                            languageComboBox.SelectedItem = item;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading user info: {ex.Message}", "User Info Load Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string iniPath = Path.Combine(settingsFolderPath, "configs.user.ini");
+                if (!File.Exists(iniPath))
+                {
+                    MessageBox.Show("INI file not found. Cannot save.", "Save Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string[] lines = File.ReadAllLines(iniPath);
+                bool foundLang = false;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string trimmed = lines[i].Trim();
+                    if (trimmed.StartsWith("account_name="))
+                    {
+                        lines[i] = "account_name=" + usernameTextBox.Text;
+                    }
+                    else if (trimmed.StartsWith("account_steamid="))
+                    {
+                        lines[i] = "account_steamid=" + steamIDTextBox.Text;
+                    }
+                    else if (trimmed.StartsWith("language="))
+                    {
+                        var selected = languageComboBox.SelectedItem as ComboBoxItem;
+                        lines[i] = "language=" + (selected != null ? selected.Code : "english");
+                        foundLang = true;
+                    }
+                }
+                if (!foundLang)
+                {
+                    var selected = languageComboBox.SelectedItem as ComboBoxItem;
+                    var langLine = "language=" + (selected != null ? selected.Code : "english");
+                    var linesList = lines.ToList();
+                    linesList.Add(langLine);
+                    lines = linesList.ToArray();
+                }
+                File.WriteAllLines(iniPath, lines);
+                MessageBox.Show("User info saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving user info: {ex.Message}", "Save Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void AvatarPanel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Title = "Select Avatar Image";
+                    ofd.Filter = "Image Files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        bool needsResize = false;
+                        using (Image img = Image.FromFile(ofd.FileName))
+                        {
+                            if (img.Width != 184 || img.Height != 184)
+                            {
+                                var result = MessageBox.Show(
+                                    "Image is not 184x184 pixels. Do you want to resize it to fit?",
+                                    "Resize Image?",
+                                    MessageBoxButtons.YesNo,
+                                    MessageBoxIcon.Question
+                                );
+                                if (result == DialogResult.Yes)
+                                {
+                                    needsResize = true;
+                                }
+                                else
+                                {
+                                    return;
+                                }
+                            }
+                        }
+
+                        if (!Directory.Exists(settingsFolderPath))
+                            Directory.CreateDirectory(settingsFolderPath);
+                        string destPath = Path.Combine(settingsFolderPath, "account_avatar.png");
+
+                        using (Image img = Image.FromFile(ofd.FileName))
+                        {
+                            Image toSave = img;
+                            if (needsResize)
+                            {
+                                Bitmap resized = new Bitmap(img, new Size(184, 184));
+                                toSave = resized;
+                            }
+                            toSave.Save(destPath, System.Drawing.Imaging.ImageFormat.Png);
+                            if (needsResize && toSave is Bitmap)
+                                toSave.Dispose();
+                        }
+
+                        LoadUserAvatar(); // Refresh avatar
+                        MessageBox.Show("Avatar updated!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating avatar: {ex.Message}", "Avatar Update Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
